bb91c8a1be2761e38f4579c62a94c452d57b18df
mello <mellomainmail@gmail.com>
2023-01-09 23:53:11 +0100

    - Initial commit

---
+++ Bukkit-Main/src/main/java/sk/m3ii0/code/bukkit/UpFantasyBooter.java
@@ -0,0 +1,22 @@
+package sk.m3ii0.code.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UpFantasyBooter extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+}
---
+++ Bukkit-Main/src/main/java/sk/m3ii0/code/bukkit/player/FantasyPlayer.java
@@ -0,0 +1,17 @@
+package sk.m3ii0.code.bukkit.player;
+
+import org.bukkit.entity.Player;
+
+public class FantasyPlayer {
+
+    private final Player player;
+
+    public FantasyPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+}
